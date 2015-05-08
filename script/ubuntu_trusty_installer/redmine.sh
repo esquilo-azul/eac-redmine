b51d7fd3d9532ff9202136c887ed2c0bca49f7fa
Eduardo Henrique Bogoni <eduardobogoni@gmail.com>
2015-05-08 16:56:31 -0300

    Instalador das dependências do Redmine para Ubuntu 14.04. Refs #25.

---
+++ script/ubuntu_trusty_installer/redmine.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -u
+set -e
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+REDMINE_ROOT=$(dirname $(dirname "$DIR"))
+
+bundleInstalled() {
+	(cd "$REDMINE_ROOT"; bundle check)	
+	return $?
+}
+
+"$DIR/apt_get_assert_packages.sh" ruby ruby-dev libmagickwand-dev
+sudo gem install -V --conservative bundler
+	
+if bundleInstalled; then
+	echo "Bundle completo"
+else
+	echo "Bundle incompleto"
+	(cd "$REDMINE_ROOT"; bundle install --without development test)
+fi
