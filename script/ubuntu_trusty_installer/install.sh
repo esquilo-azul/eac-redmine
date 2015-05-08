b232b9a9871997eb76339ba050f620cdc944720f
Eduardo Henrique Bogoni <eduardobogoni@gmail.com>
2015-05-08 17:01:30 -0300

    Nomes de scripts de instalação refatorados. Refs #25.

--- script/ubuntu_trusty_installer/install.sh
+++ script/ubuntu_trusty_installer/install.sh
@@ -32,6 +32,6 @@ if [ ! -f "$SETTINGS_FILE" ]; then
 fi
 
 source "$SETTINGS_FILE"
-$DIR/postgresql_assert_database.sh "$postgresql_database" "$postgresql_user" "$postgresql_password"
-$DIR/redmine.sh
-$DIR/apache_install.sh
+$DIR/install_postgresql.sh "$postgresql_database" "$postgresql_user" "$postgresql_password"
+$DIR/install_redmine_bundle.sh
+$DIR/install_redmine.sh
