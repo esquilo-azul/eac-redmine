74bcf2a47b2315089866911b7516b9db2511b2f2
Eduardo Henrique Bogoni <eduardobogoni@gmail.com>
2015-05-21 11:32:52 -0300

    Parser de caixa de atendimento e-Sosti compatível com o e-Admin em 20/05/2015 com solicitação sem atendente. Refs #52.

--- lib/trf1_sjap/caixa_atendimento_secao.rb
+++ lib/trf1_sjap/caixa_atendimento_secao.rb
@@ -11,13 +11,14 @@ module Trf1Sjap
 
     def solicitacoes
       data = []
-      for node in @doc.xpath("id('container_pagination')/table/tbody/tr")
-        columns = node.xpath('.//*[string-length(normalize-space(text())) > 0]/text()');
+      columns = solicitacoes_columns()
+      for line in solicitacoes_lines()
+        line_cells  = line.xpath('td')
         data.append({
-          :numero => columns[0].text.strip,
-          :solicitante => columns[2].text.strip,
-          :servico_atual => columns[3].text.strip,
-          :atendente => __parseAtendente(columns[4].text)
+          :numero => column_cell_text(line_cells[columns[:numero]]),
+          :solicitante => column_cell_text(line_cells[columns[:solicitante]]),
+          :servico_atual => column_cell_text(line_cells[columns[:servico_atual]]),
+          :atendente => __parseAtendente(column_cell_text(line_cells[columns[:atendente]]))
         })
       end
       return data
@@ -39,6 +40,58 @@ module Trf1Sjap
         end
       end
       return false
+    end
+    
+    private
+    
+    def solicitacoes_table
+      return @doc.at_xpath("id('container_pagination')/table")
+    end
+    
+    def solicitacoes_lines
+      return solicitacoes_table().xpath('tbody/tr')
+    end
+    
+    def solicitacoes_columns
+      result = {}
+      i = 0
+      for cell in solicitacoes_table().xpath('thead/tr/th')
+        column = column_by_label(column_cell_text(cell))
+        if column != nil
+          result[column] = i
+        end
+        i += 1
+      end      
+      for column in solicitacao_columns_text().keys()
+        if ! result.key?(column)
+          raise 'Coluna não encontrada: ' + column.to_s + " / Result: " + result.to_s
+        end
+      end
+      return result
+    end
+    
+    def column_by_label(label)
+      solicitacao_columns_text().each do |column, text| 
+        return column if label.downcase.include?(text)
+      end
+      return nil
+    end
+    
+    def solicitacao_columns_text
+      return {
+        :numero => 'solicitação', 
+        :solicitante => 'solicitante', 
+        :servico_atual => 'serviço',
+        :atendente => 'atendente'
+      }
+    end
+    
+    def column_cell_text(cell)
+      link_text = cell.at_xpath('a/text()')
+      if link_text != nil && link_text.to_s.strip.length > 0
+        return link_text.to_s.strip
+      end
+      return cell.text.to_s.strip
     end
 
   end
