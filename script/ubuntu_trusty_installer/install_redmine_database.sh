2f3cafe72d31a1288ffac43ddbc66a5849f7e622
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-06-23 16:42:16 -0300

    Conserta as versões de migração do plugin RedmineGitHosting. Refs #194.

--- script/ubuntu_trusty_installer/install_redmine_database.sh
+++ script/ubuntu_trusty_installer/install_redmine_database.sh
@@ -6,5 +6,6 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 REDMINE_ROOT=$(dirname $(dirname "$DIR"))
 
+"$REDMINE_ROOT/bin/bundle" exec rake trf1_sjap:fix_redmine_git_hosting_migrations
 (cd $REDMINE_ROOT; bundle exec rake db:migrate)
 (cd $REDMINE_ROOT; bundle exec rake redmine:plugins:migrate)
