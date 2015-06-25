592529538917bf150cdcbc62f223a6422a289f2d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-06-25 15:31:12 -0300

    Site Apache do Redmine em template. Refs #194.

--- script/ubuntu_trusty_installer/tasks/redmine_apache_site.sh
+++ script/ubuntu_trusty_installer/tasks/redmine_apache_site.sh
@@ -19,7 +19,7 @@ function task_condition {
 export -f task_condition
 
 function task_execute {
-	echo 'RailsBaseUri "/redmine"' | sudo tee /etc/apache2/conf-available/redmine.conf > /dev/null
+	"$INSTALL_ROOT/lib/text/template.sh" "$INSTALL_ROOT/template/redmine_apache_site.conf" | sudo tee /etc/apache2/conf-available/redmine.conf > /dev/null
 	sudo a2enconf redmine
 	sudo service apache2 reload
 }
