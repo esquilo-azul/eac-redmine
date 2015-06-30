589d51f7d1fe3cf438df782dde1805bfcec31510
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-06-30 17:14:48 -0300

    Ruby para os hooks do Gitolite. Refs #194.

---
+++ script/ubuntu_trusty_installer/tasks/apt_ruby.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -u
+set -e
+
+function task_condition {
+	return $("$INSTALL_ROOT/lib/apt/installed.sh" ruby)	
+}
+export -f task_condition
+
+function task_execute {
+	sudo apt-get install -y ruby
+}
+export -f task_execute
