ceaa09c29a43529b915474715228f59e4f20faf4
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-07-01 08:53:54 -0300

    Configuração de repositórios habilitados. Refs #194.

--- script/ubuntu_trusty_installer/tasks/all.sh
+++ script/ubuntu_trusty_installer/tasks/all.sh
@@ -4,6 +4,6 @@ set -u
 set -e
 
 function task_dependencies {
-	echo redmine_database_schema redmine_apache_site redmine_public_link redmine_git_hosting 
+	echo redmine_database_schema redmine_apache_site redmine_public_link redmine_git_hosting redmine_enabled_scm_setting
 }
 export -f task_dependencies
