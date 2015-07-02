b0aab7f6b0b9ae7b242850a63ad3e236383e2e3d
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-07-02 16:23:43 -0300

    Log de threads sem data/hora na mensagem. Refs #231.

--- lib/trf1_sjap/esosti_project_replicate/loop_thread.rb
+++ lib/trf1_sjap/esosti_project_replicate/loop_thread.rb
@@ -48,7 +48,7 @@ module Trf1Sjap
       end
 
       def log(method, message)
-        @esosti_project_replicate.logger.send(method, Time.now.strftime('%d/%m/%y %H:%M:%S') + "|" + @esosti_project_replicate.trf1_sjap_project.project.identifier + "|" + to_s + ": " + message)
+        @esosti_project_replicate.logger.send(method, @esosti_project_replicate.trf1_sjap_project.project.identifier + "|" + to_s + ": " + message)
       end
 
       def to_s
