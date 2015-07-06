8cf1924a96e1a87fb764135d7b178f89c1f77ee0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-07-06 14:58:36 -0300

    Reparo de testes. Refs #264.

--- test/unit/lib/trf1_sjap/esosti_redmine_import_test.rb
+++ test/unit/lib/trf1_sjap/esosti_redmine_import_test.rb
@@ -4,27 +4,42 @@ require File.expand_path('../../../../test_helper', __FILE__)
 
 class Trf1Sjap::EsostiRedmineImportTest < ActiveSupport::TestCase
   def test_parse_solicitacao_user
+    rotulo_parsed = EsostiUsuario.parse_esosti_usuario_rotulo('AP20103 - FÁBIO JÚNIOR LIMA DO CARMO')
     assert_equal({
-      :login => 'ap20103',
+      :matricula => 'AP20103',
+      :nome => 'FÁBIO JÚNIOR LIMA DO CARMO',
+    }, rotulo_parsed)
+    name_parsed = EsostiUsuario.parse_full_name(rotulo_parsed[:nome])
+    assert_equal({
       :firstname => 'Fábio',
       :lastname => 'Júnior Lima do Carmo'
-    }, Trf1Sjap::EsostiRedmineImport::UpdateToRedmine.parse_solicitacao_user('AP20103 - FÁBIO JÚNIOR LIMA DO CARMO'))
+    }, name_parsed)
   end
   
   def test_parse_solicitacao_user_truncate_middle
+    rotulo_parsed = EsostiUsuario.parse_esosti_usuario_rotulo('TR17631PS - LUCIANA DE FREITAS LEITE ANANIAS DA COSTA')
     assert_equal({
-      :login => 'tr17631ps',
+      :matricula => 'TR17631PS',
+      :nome => 'LUCIANA DE FREITAS LEITE ANANIAS DA COSTA',
+    }, rotulo_parsed)
+    name_parsed = EsostiUsuario.parse_full_name(rotulo_parsed[:nome])
+    assert_equal({
       :firstname => 'Luciana',
       :lastname => 'de Freitas L. Ananias da Costa'      
-    }, Trf1Sjap::EsostiRedmineImport::UpdateToRedmine.parse_solicitacao_user('TR17631PS - LUCIANA DE FREITAS LEITE ANANIAS DA COSTA'))
+    }, name_parsed)
   end
   
   def test_parse_solicitacao_user_truncate_long_name
+    rotulo_parsed = EsostiUsuario.parse_esosti_usuario_rotulo('ABC123DEF - João DE NOMEMUITOLONGOMAIORQUETRINTACARACTERES')
     assert_equal({
-      :login => 'abc123def',
+      :matricula => 'ABC123DEF',
+      :nome => 'João DE NOMEMUITOLONGOMAIORQUETRINTACARACTERES',
+    }, rotulo_parsed)
+    name_parsed = EsostiUsuario.parse_full_name(rotulo_parsed[:nome])
+    assert_equal({
       :firstname => 'João',
       :lastname => 'de N.'      
-    }, Trf1Sjap::EsostiRedmineImport::UpdateToRedmine.parse_solicitacao_user('ABC123DEF - João DE NOMEMUITOLONGOMAIORQUETRINTACARACTERES'))
+    }, name_parsed)
   end
   
   def test_parse_solicitacao_descricao
