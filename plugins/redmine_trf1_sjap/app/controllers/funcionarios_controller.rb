026ca1b5c275f0d42dc30ee72cf929daf77ab033
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-08-05 13:25:12 -0300

    Exclui os campos created_at e updated_at na listagem de funcionários. Refs #527.

--- plugins/redmine_trf1_sjap/app/controllers/funcionarios_controller.rb
+++ plugins/redmine_trf1_sjap/app/controllers/funcionarios_controller.rb
@@ -2,5 +2,7 @@ class FuncionariosController < ApplicationController
   layout 'trf1_sjap'
   before_filter :require_admin
   active_scaffold :"funcionario" do |conf|
+    conf.list.columns.exclude :created_at
+    conf.list.columns.exclude :updated_at
   end
 end
