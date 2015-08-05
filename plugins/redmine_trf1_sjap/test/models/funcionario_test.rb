c50ec541cb70f51df729686f38ef61bec33dc039
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-08-05 11:44:46 -0300

    Testa unicidade de Funcionario.cpf. Refs #527.

--- plugins/redmine_trf1_sjap/test/models/funcionario_test.rb
+++ plugins/redmine_trf1_sjap/test/models/funcionario_test.rb
@@ -27,6 +27,18 @@ class FuncionarioTest < ActiveSupport::TestCase
     assert_equal true, f.save
   end
   
+  test "test unique cpf" do
+    f = Funcionario.new
+    f.nome = "João"
+    f.cpf = '77446953560'
+    assert_equal true, f.save
+
+    f = Funcionario.new
+    f.nome = "Maria"
+    f.cpf = '77446953560'
+    assert_equal false, f.save
+  end
+
   test "test unique matricula" do
     f = Funcionario.new
     f.nome = "João"
