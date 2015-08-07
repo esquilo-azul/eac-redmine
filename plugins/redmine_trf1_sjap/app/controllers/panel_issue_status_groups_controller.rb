d5bacbfbdb87f367b7fdf6a457e6c9c8501c8b6d
Altemir Almeida Silva <altemirap25@gmail.com>
2015-08-07 16:34:51 -0300

    Inserção, atualização e exclusão de grupos de situações de tarefas. Refs #257.

--- plugins/redmine_trf1_sjap/app/controllers/panel_issue_status_groups_controller.rb
+++ plugins/redmine_trf1_sjap/app/controllers/panel_issue_status_groups_controller.rb
@@ -1,5 +1,48 @@
 class PanelIssueStatusGroupsController < ApplicationController
+  before_action :set_panel_issue_status_group, only: [:edit, :update]
+  
   def index
     @panel_issue_status_groups = PanelIssueStatusGroup.all
+  end
+  
+  def new
+  	@panel_issue_status_group = PanelIssueStatusGroup.new
+  end
+
+  def edit
+  end
+
+  def create
+    @panel_issue_status_group = PanelIssueStatusGroup.new(panel_issue_status_group_params)
+    if @panel_issue_status_group.save
+      redirect_to panel_issue_status_groups_url, notice: 'Message was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @panel_issue_status_group.update(panel_issue_status_group_params)
+      redirect_to panel_issue_status_groups_url, notice: 'Message was successfully updated'
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @panel_issue_status_group = PanelIssueStatusGroup.find(params[:id])
+    if @panel_issue_status_group.present?
+       @panel_issue_status_group.destroy
+    end
+    redirect_to panel_issue_status_groups_url, notice: 'Message fase was successfully destroyed.'
+  end
+
+  private
+    def set_panel_issue_status_group  
+      @panel_issue_status_group = PanelIssueStatusGroup.find(params[:id])
+    end
+      
+    def panel_issue_status_group_params
+      params.require(:panel_issue_status_group).permit(:name, :empty_message, :permanent)
   end
 end
--- plugins/redmine_trf1_sjap/app/models/panel_issue_status_group.rb
+++ plugins/redmine_trf1_sjap/app/models/panel_issue_status_group.rb
@@ -1,4 +1,7 @@
 # encoding: UTF-8
 class PanelIssueStatusGroup < ActiveRecord::Base
   unloadable
+  attr_accessible :name, :empty_message, :permanent
+  validates :name, presence: true
+  validates :empty_message, presence: true
 end
