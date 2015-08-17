895de55d24ec82fb5e61a47504273bcf7f04c3c8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-08-17 10:13:28 -0300

    Remoção de teste de método já não mais existente. Refs #654.

--- test/unit/lib/trf1_sjap/esosti_redmine_import_test.rb
+++ test/unit/lib/trf1_sjap/esosti_redmine_import_test.rb
@@ -41,12 +41,4 @@ class Trf1Sjap::EsostiRedmineImportTest < ActiveSupport::TestCase
       :lastname => 'de N.'      
     }, name_parsed)
   end
-  
-  def test_parse_solicitacao_descricao
-    assert_equal(
-      'Favor instalar +1 computador tipo 2 para o servidor Marco Antonio Rodrigues Lima - AP20191', 
-      Trf1Sjap::EsostiRedmineImport::UpdateToRedmine.parse_solicitacao_descricao(' + Favor instalar +1 computador tipo 2 para o servidor Marco Antonio Rodrigues Lima - AP20191 ')
-    )
-  end
-
 end
