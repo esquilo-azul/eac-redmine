7edfa97b0149d5badc259a0d8e5162e5615c0239
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-09-10 10:13:25 -0300

    Remove obrigatoriedade de usuário administrador em PontoEntradasController. Refs #731.

--- plugins/sjap_ponto/app/controllers/ponto_entradas_controller.rb
+++ plugins/sjap_ponto/app/controllers/ponto_entradas_controller.rb
@@ -1,6 +1,5 @@
 class PontoEntradasController < ApplicationController
   layout 'trf1_sjap'
-  before_filter :require_admin
   active_scaffold :ponto_entrada do |conf|
     conf.columns[:data_hora].form_ui = :datetime_picker
     conf.columns[:autor].form_ui = :select
