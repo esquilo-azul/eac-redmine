b8d257cd21a190d9793e8fafdfd5f3225506a0ec
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-09-28 14:32:11 -0300

    Adiciona "fuso horário" em PontoTerminal. Refs #907.

--- plugins/sjap_ponto/app/models/ponto_terminal.rb
+++ plugins/sjap_ponto/app/models/ponto_terminal.rb
@@ -8,6 +8,7 @@ class PontoTerminal < ActiveRecord::Base
   validates :endereco, presence: true
   validates :usuario, presence: true
   validates :senha, presence: true
+  validates :fuso_horario, presence: true, format: { with: /[\-\+]\d{2}\:\d{2}/ }
 
   def to_s
     descricao
