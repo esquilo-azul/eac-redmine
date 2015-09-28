8d6574fd7a61552d35564844c906322a8e647a0b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-09-28 08:45:52 -0300

    Omite campo "cef_id_solicitacoes_ultima_consulta" na edição de funcionário. Refs #908.

--- plugins/redmine_trf1_sjap/app/controllers/funcionarios_controller.rb
+++ plugins/redmine_trf1_sjap/app/controllers/funcionarios_controller.rb
@@ -4,5 +4,7 @@ class FuncionariosController < ApplicationController
   active_scaffold :funcionario do |conf|
     conf.list.columns.exclude :created_at
     conf.list.columns.exclude :updated_at
+    conf.create.columns.exclude :cef_id_solicitacoes_ultima_consulta
+    conf.update.columns.exclude :cef_id_solicitacoes_ultima_consulta
   end
 end
