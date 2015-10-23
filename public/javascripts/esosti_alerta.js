004703dcb65482fc9caec55da0a9613eefc2838b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-10-23 13:19:01 -0300

    Formatação de código. Refs #911.

--- public/javascripts/esosti_alerta.js
+++ public/javascripts/esosti_alerta.js
@@ -3,32 +3,32 @@ function EsostiAlerta() {
 
 EsostiAlerta.ESOSTI_ALERTA_DATA_PATH = undefined;
 
-EsostiAlerta.start = function() {
-	$.post(EsostiAlerta.ESOSTI_ALERTA_DATA_PATH, {
-		matricula : $('#matricula').val(),
-		senha : $('#senha').val(),
-		banco : $('#banco').val(),
-	}).success(function(data) {
-		$('#resultContainer').html(data);
-	}).always(function(data) {
-		EsostiAlerta.__countdownTime = 15;
-	}).always(function(data) {
-		EsostiAlerta.__countdownTime = $('#intervalo').val();
-		EsostiAlerta.__countdown();
-	});
+EsostiAlerta.start = function () {
+  $.post(EsostiAlerta.ESOSTI_ALERTA_DATA_PATH, {
+    matricula: $('#matricula').val(),
+    senha: $('#senha').val(),
+    banco: $('#banco').val(),
+  }).success(function (data) {
+    $('#resultContainer').html(data);
+  }).always(function (data) {
+    EsostiAlerta.__countdownTime = 15;
+  }).always(function (data) {
+    EsostiAlerta.__countdownTime = $('#intervalo').val();
+    EsostiAlerta.__countdown();
+  });
 };
 
-EsostiAlerta.__countdown = function() {
-	if (EsostiAlerta.__countdownTime <= 0) {
-		$('#updateStatus').html("Atualizando...");
-		EsostiAlerta.start();
-	} else {
-		$('#updateStatus').html("Atualização em " + EsostiAlerta.__countdownTime + " segundo(s)");
-		EsostiAlerta.__countdownTime -= 1;
-		setTimeout(EsostiAlerta.__countdown, 1000);
-	}
-}; 
+EsostiAlerta.__countdown = function () {
+  if (EsostiAlerta.__countdownTime <= 0) {
+    $('#updateStatus').html("Atualizando...");
+    EsostiAlerta.start();
+  } else {
+    $('#updateStatus').html("Atualização em " + EsostiAlerta.__countdownTime + " segundo(s)");
+    EsostiAlerta.__countdownTime -= 1;
+    setTimeout(EsostiAlerta.__countdown, 1000);
+  }
+};
 
-EsostiAlerta.update = function(){
-	EsostiAlerta.__countdownTime = 0;	
+EsostiAlerta.update = function () {
+  EsostiAlerta.__countdownTime = 0;
 };
