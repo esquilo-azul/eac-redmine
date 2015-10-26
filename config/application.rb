152e09d6d86347b60aae520b941543d084e58887
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-10-26 09:41:57 -0300

    Não mostra campos "senha" no log. Refs #911.

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module RedmineApp
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :senha]
 
     # Enable the asset pipeline
     config.assets.enabled = false
