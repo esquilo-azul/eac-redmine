04925a06d00098f095725a629b377832cd7b4d47
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-11-16 15:36:04 -0300

    Paginação da lista de cargas horárias. Closes #906.

--- plugins/sjap_ponto/app/controllers/ponto_carga_horarias_controller/index.rb
+++ plugins/sjap_ponto/app/controllers/ponto_carga_horarias_controller/index.rb
@@ -1,19 +1,32 @@
 class PontoCargaHorariasController < ApplicationController
   module Index
-    def index
-      @funcionario = nil
-      @funcionario = Funcionario.find(params['funcionario']) unless params['funcionario'].blank?
-      @ponto_carga_horarias = index_query.order(data_inicial: :desc, data_final: :asc,
-                                                created_at: :desc).all
+    extend ActiveSupport::Concern
+
+    included do
+      include SortHelper
+      helper :sort
     end
 
-    def index_query
-      query = PontoCargaHoraria
+    def index
+      sort_init 'data_inicial'
+      sort_update %w(data_inicial data_final descricao minutos_continuo minutos_descontinuo)
+      @funcionario = nil
+      @funcionario = Funcionario.find(params['funcionario']) unless params['funcionario'].blank?
+      @limit = per_page_option
+      s = scope
+      @rows_count = s.count
+      @rows_pages = Redmine::Pagination::Paginator.new @rows_count, @limit, params['page']
+      @offset = @rows_pages.offset
+      @rows = s.order(sort_clause).limit(@limit).offset(@offset).to_a
+    end
+
+    def scope
+      s = PontoCargaHoraria
       if @funcionario
-        query = query.includes(:funcionarios).where(ponto_carga_horaria_funcionarios:
+        s = s.includes(:funcionarios).where(ponto_carga_horaria_funcionarios:
             { funcionario_id: @funcionario.id })
       end
-      query
+      s
     end
   end
 end
