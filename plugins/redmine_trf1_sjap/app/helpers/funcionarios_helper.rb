bae6b8b653d1b6769ebdb64211f30f786f99a5ef
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-12-03 11:19:13 -0300

    Seletor múltiplo de funcionários. Refs #915.

---
+++ plugins/redmine_trf1_sjap/app/helpers/funcionarios_helper.rb
@@ -0,0 +1,123 @@
+module FuncionariosHelper
+  def multi_funcionario_input(form, column)
+    MultiFuncionarioInput.new(self, form, column).build
+  end
+
+  private
+
+  class MultiFuncionarioInput
+    def initialize(view, form, column)
+      @view = view
+      @form = form
+      @column = column
+    end
+
+    def build
+      @view.content_tag(:span, id: id, class: 'FuncionariosHelper') do
+        type_select <<
+          @view.tag(:br) <<
+          single_input <<
+          multi_input <<
+          @view.content_tag(:script, @view.raw("new FuncionariosHelper('#{id}', " \
+              "'#{result_input_name}')"))
+      end
+    end
+
+    private
+
+    def result_input_name
+      "#{@form.object_name}[#{sanitized_method_name}][]"
+    end
+
+    def sanitized_method_name
+      @column.to_s.sub(/\?$/, '')
+    end
+
+    def id
+      'funcionarios_helper_multi_funcionario_input'
+    end
+
+    def type_select
+      b = ActiveSupport::SafeBuffer.new
+      { 'single' => 'Único', 'multi' => 'Vários' }.each do |k, v|
+        b << @view.content_tag(:span) do
+          @view.radio_button_tag('multi_funcionario_type', k,
+                                 (@view.params['multi_funcionario_type'] || 'single') == k) << v
+        end
+      end
+      b
+    end
+
+    def single_input
+      @view.content_tag(:span, class: 'single_container') do
+        field_name = "#{@column}_single_funcionario_id"
+        options = @view.options_from_collection_for_select(Funcionario.order(nome: :asc).all, :id,
+                                                           :nome, @view.params[field_name])
+        @view.select_tag(field_name, options)
+      end
+    end
+
+    def multi_input
+      @view.content_tag(:span, class: 'multi_container') do
+        b = ActiveSupport::SafeBuffer.new
+        funcionarios_grouped.each do |k, v|
+          b << funcionarios_group_container(k, v)
+        end
+        b
+      end
+    end
+
+    def funcionarios_grouped
+      groups = {}
+      Funcionario.order(nome: :asc).all.each do |f|
+        letter = f.nome.upcase[0]
+        groups[letter] ||= []
+        groups[letter] << f
+      end
+      groups
+    end
+
+    def funcionarios_group_container(letter, funcionarios)
+      @view.content_tag(:span, class: 'funcionarios_group_container') do
+        b = @view.content_tag(:span, letter, class: 'letter')
+        b << @view.content_tag(:span, class: 'funcionarios_container') do
+          funcionarios_rows(funcionarios)
+        end
+      end
+    end
+
+    def funcionarios_rows(funcionarios)
+      b = ActiveSupport::SafeBuffer.new
+      RowsBuilder.new(4, funcionarios).rows.each do |r|
+        b << @view.content_tag(:span, class: 'row') do
+          b1 = ActiveSupport::SafeBuffer.new
+          r.each { |f| b1 << funcionario(f) }
+          b1
+        end
+      end
+      b
+    end
+
+    def funcionario(f)
+      @view.content_tag(:span, class: 'funcionario') do
+        field_name = "#{@column}_multi_funcionario_#{f.id}"
+        @view.check_box_tag(field_name, '1', @view.params[field_name] == '1') << f.to_s
+      end
+    end
+
+    class RowsBuilder
+      def initialize(row_size, funcionarios)
+        @row_size = row_size
+        @rows = []
+        funcionarios.each { |f| add_funcionario(f) }
+      end
+
+      def add_funcionario(f)
+        @rows << [] if @rows.empty? || @rows[-1].size >= @row_size
+        @rows[-1] << f
+      end
+
+      attr_reader :rows
+    end
+  end
+end
---
+++ public/javascripts/funcionarios_helper.js
@@ -0,0 +1,82 @@
+function FuncionariosHelper(root_id, result_input_name) {
+  this.root_id = root_id;
+  this.result_input_name = result_input_name;
+  var THIS = this;
+  $(document).ready(function () {
+    THIS.__form().submit(function (event) {
+      try {
+        THIS.__addValuesToForm();
+      }
+      catch (ex) {
+        event.preventDefault();
+        alert(ex);
+      }
+    });
+    THIS.__root().find('input:radio[name="multi_funcionario_type"]').change(function () {
+      THIS.update();
+    });
+    THIS.update();
+  });
+}
+
+FuncionariosHelper.prototype.update = function () {
+  if (this.__type() == 'single') {
+    this.__singleContainer().show();
+    this.__multiContainer().hide();
+  }
+  else {
+    this.__singleContainer().hide();
+    this.__multiContainer().show();
+  }
+}
+
+FuncionariosHelper.prototype.__type = function () {
+  return this.__root().find('input[name="multi_funcionario_type"]:checked').val();
+}
+
+FuncionariosHelper.prototype.__root = function () {
+  return $('#' + this.root_id);
+}
+
+FuncionariosHelper.prototype.__singleContainer = function () {
+  return this.__root().find('.single_container');
+}
+
+FuncionariosHelper.prototype.__multiContainer = function () {
+  return this.__root().find('.multi_container');
+}
+
+FuncionariosHelper.prototype.__form = function () {
+  return this.__root().closest('form');
+}
+
+FuncionariosHelper.prototype.__addValuesToForm = function (form) {
+  var ids = this.__type() == 'single' ? this.__singleIds() : this.__multiIds();
+  var THIS = this;
+  $.each(ids, function (i, v) {
+    $(THIS.__form()).append($('<input />')
+            .attr('type', 'hidden')
+            .attr('name', THIS.result_input_name)
+            .val(v));
+  });
+}
+
+FuncionariosHelper.prototype.__singleIds = function () {
+  return [this.__root().find('.single_container select').val()];
+}
+
+FuncionariosHelper.prototype.__multiIds = function () {
+  var ids = [];
+  this.__root().find('.funcionario > input[type="checkbox"]').each(function () {
+    if ($(this).is(':checked')) {
+      var id = /\d+$/.exec($(this).attr('name'))
+      if (id) {
+        ids.push(id[0]);
+      }
+      else {
+        throw 'Falhou ao tentar extrair o ID de "' + $(this).attr('name') + '"';
+      }
+    }
+  });
+  return ids;
+}
