f9ca58f8c9a3770b747f2739d262dfe119165e77
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2015-12-03 12:20:06 -0300

    Refatora FuncionariosHelper. Refs #915.

--- plugins/redmine_trf1_sjap/app/helpers/funcionarios_helper.rb
+++ plugins/redmine_trf1_sjap/app/helpers/funcionarios_helper.rb
@@ -5,7 +5,88 @@ module FuncionariosHelper
 
   private
 
+  module Single
+    def single_input
+      view.content_tag(:span, class: 'single_container') do
+        field_name = "#{column}_single_funcionario_id"
+        options = view.options_from_collection_for_select(Funcionario.order(nome: :asc).all, :id,
+                                                          :nome, view.params[field_name])
+        view.select_tag(field_name, options)
+      end
+    end
+  end
+
+  module Multi
+    def multi_input
+      view.content_tag(:span, class: 'multi_container') do
+        b = ActiveSupport::SafeBuffer.new
+        funcionarios_grouped.each do |k, v|
+          b << funcionarios_group_container(k, v)
+        end
+        b
+      end
+    end
+
+    def funcionarios_grouped
+      groups = {}
+      Funcionario.order(nome: :asc).all.each do |f|
+        letter = f.nome.upcase[0]
+        groups[letter] ||= []
+        groups[letter] << f
+      end
+      groups
+    end
+
+    def funcionarios_group_container(letter, funcionarios)
+      view.content_tag(:span, class: 'funcionarios_group_container') do
+        b = view.content_tag(:span, letter, class: 'letter')
+        b << view.content_tag(:span, class: 'funcionarios_container') do
+          funcionarios_rows(funcionarios)
+        end
+      end
+    end
+
+    def funcionarios_rows(funcionarios)
+      b = ActiveSupport::SafeBuffer.new
+      RowsBuilder.new(4, funcionarios).rows.each do |r|
+        b << view.content_tag(:span, class: 'row') do
+          b1 = ActiveSupport::SafeBuffer.new
+          r.each { |f| b1 << funcionario(f) }
+          b1
+        end
+      end
+      b
+    end
+
+    def funcionario(f)
+      view.content_tag(:span, class: 'funcionario') do
+        field_name = "#{@column}_multi_funcionario_#{f.id}"
+        view.check_box_tag(field_name, '1', view.params[field_name] == '1') << f.to_s
+      end
+    end
+
+    class RowsBuilder
+      def initialize(row_size, funcionarios)
+        @row_size = row_size
+        @rows = []
+        funcionarios.each { |f| add_funcionario(f) }
+      end
+
+      def add_funcionario(f)
+        @rows << [] if @rows.empty? || @rows[-1].size >= @row_size
+        @rows[-1] << f
+      end
+
+      attr_reader :rows
+    end
+  end
+
   class MultiFuncionarioInput
+    include Single
+    include Multi
+
+    attr_reader :view, :form, :column
+
     def initialize(view, form, column)
       @view = view
       @form = form
@@ -46,78 +127,6 @@ module FuncionariosHelper
         end
       end
       b
-    end
-
-    def single_input
-      @view.content_tag(:span, class: 'single_container') do
-        field_name = "#{@column}_single_funcionario_id"
-        options = @view.options_from_collection_for_select(Funcionario.order(nome: :asc).all, :id,
-                                                           :nome, @view.params[field_name])
-        @view.select_tag(field_name, options)
-      end
-    end
-
-    def multi_input
-      @view.content_tag(:span, class: 'multi_container') do
-        b = ActiveSupport::SafeBuffer.new
-        funcionarios_grouped.each do |k, v|
-          b << funcionarios_group_container(k, v)
-        end
-        b
-      end
-    end
-
-    def funcionarios_grouped
-      groups = {}
-      Funcionario.order(nome: :asc).all.each do |f|
-        letter = f.nome.upcase[0]
-        groups[letter] ||= []
-        groups[letter] << f
-      end
-      groups
-    end
-
-    def funcionarios_group_container(letter, funcionarios)
-      @view.content_tag(:span, class: 'funcionarios_group_container') do
-        b = @view.content_tag(:span, letter, class: 'letter')
-        b << @view.content_tag(:span, class: 'funcionarios_container') do
-          funcionarios_rows(funcionarios)
-        end
-      end
-    end
-
-    def funcionarios_rows(funcionarios)
-      b = ActiveSupport::SafeBuffer.new
-      RowsBuilder.new(4, funcionarios).rows.each do |r|
-        b << @view.content_tag(:span, class: 'row') do
-          b1 = ActiveSupport::SafeBuffer.new
-          r.each { |f| b1 << funcionario(f) }
-          b1
-        end
-      end
-      b
-    end
-
-    def funcionario(f)
-      @view.content_tag(:span, class: 'funcionario') do
-        field_name = "#{@column}_multi_funcionario_#{f.id}"
-        @view.check_box_tag(field_name, '1', @view.params[field_name] == '1') << f.to_s
-      end
-    end
-
-    class RowsBuilder
-      def initialize(row_size, funcionarios)
-        @row_size = row_size
-        @rows = []
-        funcionarios.each { |f| add_funcionario(f) }
-      end
-
-      def add_funcionario(f)
-        @rows << [] if @rows.empty? || @rows[-1].size >= @row_size
-        @rows[-1] << f
-      end
-
-      attr_reader :rows
     end
   end
 end
