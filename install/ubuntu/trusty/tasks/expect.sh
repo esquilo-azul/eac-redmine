9789d95e9fae232003a39188a134562372a6cb0e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-05-22 13:41:22 -0300

    Instala o programa "expect". Refs #228.

---
+++ install/ubuntu/trusty/tasks/expect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -u
+set -e
+
+function task_condition {
+  return $("$INSTALL_ROOT/lib/apt/installed.sh" expect)
+}
+export -f task_condition
+
+function task_execute {
+  sudo apt-get install -y expect
+}
+export -f task_execute
