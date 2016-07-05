7bab984d9cd45b4f73bc49231c82ceea29e29cc3
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-07-04 23:17:37 -0300

    Move EacBase::Patches::JournalPatch para EventsManager::Patches::JournalPatch. Refs #408.

--- plugins/events_manager/lib/events_manager/patches/journal_patch.rb
+++ plugins/events_manager/lib/events_manager/patches/journal_patch.rb
@@ -1,4 +1,4 @@
-module EacBase
+module EventsManager
   module Patches
     module JournalPatch
       def self.included(base)
@@ -14,13 +14,13 @@ module EacBase
       module InstanceMethods
         def journal_create_event
           return unless journalized_type == 'Issue'
-          EacBase::EventManager.trigger(Issue, :update, self)
+          EventsManager::EventManager.trigger(Issue, :update, self)
         end
       end
     end
   end
 end
 
-unless Journal.included_modules.include? EacBase::Patches::JournalPatch
-  Journal.send(:include, EacBase::Patches::JournalPatch)
+unless Journal.included_modules.include? EventsManager::Patches::JournalPatch
+  Journal.send(:include, EventsManager::Patches::JournalPatch)
 end
