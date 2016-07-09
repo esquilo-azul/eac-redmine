dfcae6b271024f701b6041a9e581be06afe7e209
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-07-08 23:21:06 -0300

    Não-envio de notificações Telegram próprias. Refs #464.

--- plugins/heroku/lib/heroku/application_deploy.rb
+++ plugins/heroku/lib/heroku/application_deploy.rb
@@ -99,7 +99,7 @@ module Heroku
     end
 
     def telegram_notify(b)
-      Notifyme::Notify.notify(content_type: :plain, content: b.to_s)
+      Notifyme::Notify.notify(content_type: :plain, content: b.to_s, author: nil)
     end
 
     def workcopy
