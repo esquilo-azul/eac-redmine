218d3fd2fb3125b72ca26504959822a8582a0700
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2016-12-26 17:20:35 -0200

    Install Git for Ubuntu 14.04.

--- install/ubuntu_trusty/tasks/redmine_bundle.sh
+++ install/ubuntu_trusty/tasks/redmine_bundle.sh
@@ -16,7 +16,7 @@ export -f task_condition
 function task_execute {
 	set +e
 	# Dependências para o Redmine
-	"$INSTALL_ROOT/lib/apt/assert_installed.sh" libmagickwand-dev libxslt1-dev libpq-dev imagemagick 
+	"$INSTALL_ROOT/lib/apt/assert_installed.sh" libmagickwand-dev libxslt1-dev libpq-dev imagemagick git
 	# Dependências para o RedmineGitHosting
 	"$INSTALL_ROOT/lib/apt/assert_installed.sh" build-essential libssh2-1 libssh2-1-dev cmake libgpg-error-dev
 	"$REDMINE_ROOT/bin/bundle" install || "$REDMINE_ROOT/bin/bundle" update
