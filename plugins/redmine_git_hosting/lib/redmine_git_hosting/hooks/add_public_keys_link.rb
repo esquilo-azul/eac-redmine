33bb62f44e9982ec6571a9b73feb3599a4decb90
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2017-01-24 23:26:38 -0200

    Fixes "My public keys" link in "My account" when URL has path.

--- plugins/redmine_git_hosting/lib/redmine_git_hosting/hooks/add_public_keys_link.rb
+++ plugins/redmine_git_hosting/lib/redmine_git_hosting/hooks/add_public_keys_link.rb
@@ -4,7 +4,7 @@ module RedmineGitHosting
 
       def view_my_account_contextual(context)
         user = context[:user]
-        link_to(l(:label_my_public_keys), public_keys_path, class: 'icon icon-passwd') if user.allowed_to_create_ssh_keys?
+        link_to(l(:label_my_public_keys), { controller: 'gitolite_public_keys' }, class: 'icon icon-passwd') if user.allowed_to_create_ssh_keys?
       end
 
       def self.default_url_options
