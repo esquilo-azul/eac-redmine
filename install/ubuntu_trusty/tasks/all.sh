73c67dbdbb15d57322e8f22563191336594dc028
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2018-03-14 13:55:14 -0300

    Instalador: renomeia task "redmine_apache_site" para "redmine_as_apache_path". Refs #598.

--- install/ubuntu_trusty/tasks/all.sh
+++ install/ubuntu_trusty/tasks/all.sh
@@ -4,6 +4,6 @@ set -u
 set -e
 
 function task_dependencies {
-  echo development redmine_apache_site redmine_public_link
+  echo development redmine_as_apache_path redmine_public_link
 }
 export -f task_dependencies
