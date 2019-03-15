acbc64587a010d574423eb8237d0da2f377df475
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-03-15 17:42:39 -0300

    Installer: "lib/linux/service_running" próprio para Ubuntu Bionic.

--- install/ubuntu_bionic/lib/linux/service_running.sh
+++ install/ubuntu_bionic/lib/linux/service_running.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
 set -u
-set -e
-
-RESULT=$(service "$1" status)
 
 set +e
-echo $RESULT | grep -i down > /dev/null
-DOWN=$?
-echo $RESULT | grep -i online > /dev/null
-UP=$?
+sudo service "$1" status > /dev/null 2> /dev/null
+RESULT=$?
 set -e
 
-if [ "$UP" -eq 0 ]; then
-  echo '0'
-  exit 0
-fi
-
-if [ "$DOWN" -eq 0 ]; then
-  echo '1'
-  exit 0
-fi
-
-2>&1 echo "Status unknown: $RESULT"
-exit 1
+echo "$RESULT"
