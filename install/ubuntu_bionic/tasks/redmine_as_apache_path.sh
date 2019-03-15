68c682be6ad014f6345823306f6dc45181b5b71a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-03-15 18:30:49 -0300

    Installer: verifica conteúdo da configuração de site Apache.

--- install/ubuntu_bionic/tasks/redmine_as_apache_path.sh
+++ install/ubuntu_bionic/tasks/redmine_as_apache_path.sh
@@ -3,6 +3,8 @@
 set -u
 set -e
 
+TEMPLATE="$INSTALL_ROOT/template/redmine_as_apache_path.conf"
+
 function task_dependencies {
   echo redmine_as_apache_base redmine_public_link
 }
@@ -15,11 +17,12 @@ function task_condition {
   if [ ! -f /etc/apache2/conf-enabled/redmine.conf ]; then
     return 1
   fi
+  return $("$INSTALL_ROOT/lib/text/template.sh" "$TEMPLATE" | "$INSTALL_ROOT/lib/text/diff-stdin-file.sh" "/etc/apache2/conf-available/redmine.conf" )
 }
 export -f task_condition
 
 function task_execute {
-  "$INSTALL_ROOT/lib/text/template.sh" "$INSTALL_ROOT/template/redmine_as_apache_path.conf" | sudo tee /etc/apache2/conf-available/redmine.conf > /dev/null
+  "$INSTALL_ROOT/lib/text/template.sh" "$TEMPLATE" | sudo tee /etc/apache2/conf-available/redmine.conf > /dev/null
   sudo a2enconf redmine
 }
 export -f task_execute
