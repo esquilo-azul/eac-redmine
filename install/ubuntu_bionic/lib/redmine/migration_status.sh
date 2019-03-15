eb97f050e0e0645dddcd565c46258af445088f08
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-03-15 17:42:59 -0300

    Installer: "lib/redmine/migration_status" verifica migrações de plugins.

--- install/ubuntu_bionic/lib/redmine/migration_status.sh
+++ install/ubuntu_bionic/lib/redmine/migration_status.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
-set +e
+set -e
+set -u
 
-function migration_status {
-  "$INSTALL_ROOT/lib/rails/rake.sh" db:migrate:status 2> /dev/null
+function migration_status() {
+  local content="$("$INSTALL_ROOT/lib/rails/rake.sh" "$1" 2> /dev/null)"
+  set +e
+  echo "$content" | grep '^\s*down\s' > /dev/null 2> /dev/null
+  down=$?
+  echo "$content" | grep '^\s*up\s' > /dev/null 2> /dev/null
+  up=$?
+  set -e
+  if [ "$down" -eq 0 -o "$up" -ne 0 ]; then
+    echo 1
+  else
+    echo 0
+  fi
 }
 
-migration_status | grep '^\s*down\s' > /dev/null 2> /dev/null
-down=$?
-migration_status | grep '^\s*up\s' > /dev/null 2> /dev/null
-up=$?
-if [ "$down" -eq 0 -o "$up" -ne 0 ]; then
+if [ "$(migration_status 'db:migrate:status')" != '0' ]; then
   echo 1
-else
-  echo 0
+  exit
 fi
+if [ "$(migration_status 'redmine:plugins:migrate:status')" != '0' ]; then
+  echo 1
+  exit
+fi
+echo 0
