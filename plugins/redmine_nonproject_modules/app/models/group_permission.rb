9976fde62529b5e8d83782392d0f72c4b199dd11
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-04-05 12:51:44 -0300

    Subrepo plugins/redmine_nonprojects_modules (https://github.com/esquilo-azul/redmine_nonproject_modules).
    subrepo:
      subdir:   "plugins/redmine_nonproject_modules"
      merged:   "4d9d53e79"
    upstream:
      origin:   "https://github.com/esquilo-azul/redmine_nonproject_modules"
      branch:   "master"
      commit:   "4d9d53e79"
    git-subrepo:
      version:  "0.4.0"
      origin:   "???"
      commit:   "???"

---
+++ plugins/redmine_nonproject_modules/app/models/group_permission.rb
@@ -0,0 +1,104 @@
+class GroupPermission < ActiveRecord::Base
+  belongs_to :group
+
+  validates :group, presence: true
+  validates :permission, presence: true, uniqueness: { scope: [:group],
+                                                       case_sensitive: false }
+
+  def permission=(value)
+    self[:permission] = Permission.sanitize_key(value)
+  end
+
+  class << self
+    def permissions
+      permissions_hash.values
+    end
+
+    def add_permission(key, options = {})
+      p = Permission.new(key, options)
+      permissions_hash[p.key] = p unless permissions_hash.key?(p.key)
+    end
+
+    def permission_description(key)
+      permission(key).description
+    end
+
+    def permission?(permission, user = false)
+      return permission_by_hash?(permission, user) if permission.is_a?(Hash)
+      permission(permission).user_has?(user || User.current)
+    end
+
+    def permission(key)
+      key = key.to_s
+      return permissions_hash[key] if permissions_hash.key?(key)
+      raise "Not found \"#{key}\" in GroupPermission::permissions"
+    end
+
+    private
+
+    def permissions_hash
+      @permissions ||= {}.with_indifferent_access
+    end
+
+    def permission_by_hash?(hash, user)
+      raise 'Hasy should have :or parameter' unless hash[:or].present?
+      ps = hash[:or].is_a?(Array) ? hash[:or] : [hash[:or]]
+      ps.any? { |p| permission?(p, user) }
+    end
+  end
+
+  class Permission
+    include ::Eac::SimpleCache
+
+    class << self
+      def sanitize_key(k)
+        k.to_s.downcase
+      end
+    end
+
+    attr_reader :key, :dependencies
+
+    def initialize(key, options)
+      @key = self.class.sanitize_key(key)
+      @dependencies = (options[:dependencies] || []).map { |k| ::GroupPermission.permission(k) }
+    end
+
+    def description
+      I18n.t("permission_#{key}_description")
+    end
+
+    def to_s
+      key
+    end
+
+    def user_has?(user)
+      return true if user.admin
+      GroupPermission.where(group: user_groups(user), permission: depends_recursive.to_a).any?
+    end
+
+    def depends_recursive(visited = Set.new)
+      return [] if visited.include?(key)
+      r = Set.new([key])
+      visited << key
+      depends.each do |d|
+        r += d.depends_recursive(visited)
+      end
+      r
+    end
+
+    def <=>(other)
+      to_s <=> other.to_s
+    end
+
+    private
+
+    def user_groups(user)
+      return [Group.anonymous] if user.anonymous?
+      [Group.anonymous, Group.non_member] + user.groups
+    end
+
+    def depends
+      ::GroupPermission.permissions.select { |p| p.dependencies.include?(self) }
+    end
+  end
+end
---
+++ plugins/redmine_nonproject_modules/init.rb
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+require 'redmine'
+
+require 'redmine_nonproject_modules/patches/controller_patch'
+require 'redmine_nonproject_modules/patches/group_patch'
+require 'redmine_nonproject_modules/patches/redmine/menu_manager_patch'
+
+Redmine::Plugin.register :redmine_nonproject_modules do
+  name 'Redmine non-project modules'
+  author 'Eduardo Henrique Bogoni'
+  description 'Customizações para a SJAP'
+  version '0.1.0'
+  url 'https://github.com/eduardobogoni/redmine_nonproject_modules'
+  author_url 'https://github.com/eduardobogoni'
+
+  Redmine::MenuManager.map :admin_menu do |menu|
+    menu.push :group_permissions, { controller: 'group_permissions', action: 'index' },
+              caption: :label_group_permission_plural, after: :groups,
+              if: proc { GroupPermission.permission?('group_permissions') }
+  end
+
+  ActiveScaffold.delayed_setup = true
+end
+
+Rails.configuration.to_prepare do
+  GroupPermission.add_permission('group_permissions')
+  GroupPermission.add_permission('group_merge')
+  require_dependency 'redmine_nonproject_modules/patches/user_patch'
+end
