9bf28efa709cdbd994c75f3f3d42ba8293d359a8
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-04-06 19:14:49 -0300

    Bump to '0.1.1'.

--- plugins/heroku/init.rb
+++ plugins/heroku/init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :heroku do
   name 'Heroku'
   author 'Eduardo Henrique Bogoni'
   description ''
-  version '0.1.0'
+  version '0.1.1'
 
   Redmine::MenuManager.map :admin_menu do |menu|
     menu.push :heroku_accounts, { controller: 'heroku_accounts', action: 'index' },
