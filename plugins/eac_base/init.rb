028a2f007794f00445efae058c26259cc24a4376
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-04-06 19:16:57 -0300

    Bump to '0.1.1'.

--- plugins/eac_base/init.rb
+++ plugins/eac_base/init.rb
@@ -6,5 +6,5 @@ Redmine::Plugin.register :eac_base do
   name 'Esquilo Azul Company - Base'
   author 'Eduardo Henrique Bogoni'
   description 'Base dos plugins para a Esquilo Azul Company'
-  version '0.1.0'
+  version '0.1.1'
 end
