b6056abf706558638ad6ca0f8ed939229b145011
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-03 17:29:23 -0300

    installer/tasks/python_two.

--- plugins/redmine_with_git/installer/tasks/python_two.sh
+++ plugins/redmine_with_git/installer/tasks/python_two.sh
@@ -3,12 +3,11 @@
 set -u
 set -e
 
-PYTHON2_PACKAGE='python-minimal'
+export PYTHON2_PACKAGE='python-minimal'
 
 function task_condition {
-  programeiro /apt/installed "$PYTHON2_PACKAGE"
+  return $(programeiro /apt/installed "$PYTHON2_PACKAGE")
 }
-export -f task_condition
 
 function task_fix {
   programeiro /apt/assert_installed "$PYTHON2_PACKAGE"
