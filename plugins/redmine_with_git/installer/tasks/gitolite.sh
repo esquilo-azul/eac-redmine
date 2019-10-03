81d87cb3c20e2420c407cceac9da507b82d3974e
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-03 18:29:11 -0300

    installer/tasks/gitolite.

--- plugins/redmine_with_git/installer/tasks/gitolite.sh
+++ plugins/redmine_with_git/installer/tasks/gitolite.sh
@@ -3,10 +3,10 @@
 set -u
 set -e
 
-GITOLITE_PACKAGE=gitolite3
+export GITOLITE_PACKAGE='gitolite3'
 
 function task_condition {
-  programeiro /apt/installed "$GITOLITE_PACKAGE"
+  return $(programeiro /apt/installed "$GITOLITE_PACKAGE")
 }
 
 function task_fix {
