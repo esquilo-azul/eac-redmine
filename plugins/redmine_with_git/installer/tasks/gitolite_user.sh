98a97de44b8906acd24cf71eb0a75dd0d639aba0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2019-10-03 18:49:08 -0300

    installer/tasks/gitolite_user.

--- plugins/redmine_with_git/installer/tasks/gitolite_user.sh
+++ plugins/redmine_with_git/installer/tasks/gitolite_user.sh
@@ -4,13 +4,8 @@ set -u
 set -e
 
 function task_condition {
-  if id -u "$gitolite_user" >/dev/null 2>&1; then
-    return 0
-  else
-    return 1
-  fi
+  id -u "$gitolite_user" >/dev/null 2>&1
 }
-export -f task_condition
 
 function task_fix {
   sudo useradd --system "$gitolite_user"
