cbb20be7053039e117a1d7581e81c744019fb31a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2020-08-15 11:06:21 -0300

    Plugin "eac_redmine_base0": atualiza para '0.4.0'.

--- plugins/eac_redmine_base0/lib/eac_redmine_base0/plugins_test.rb
+++ plugins/eac_redmine_base0/lib/eac_redmine_base0/plugins_test.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
 
-require 'avm/result'
-require 'eac_ruby_gems_utils/gem'
 require 'eac_ruby_utils/console/speaker'
 require 'eac_ruby_utils/envs'
-require 'eac_ruby_utils/simple_cache'
 
 module EacRedmineBase0
   class PluginsTest
     include ::EacRubyUtils::Console::Speaker
 
     def run
-      @plugins = []
+      @tests = []
       ::Redmine::Plugin.registered_plugins.each_value do |plugin|
         check_plugin(plugin)
       end
@@ -19,91 +16,34 @@ module EacRedmineBase0
     end
 
     def check_plugin(plugin)
-      infom "Checking plugin \"#{plugin.id}\"..."
-      test_plugin = PluginTest.new(plugin)
-      test_plugin.test_result
-      infom "Plugin \"#{plugin.id}\" checked"
-      @plugins << test_plugin
+      ::EacRedmineBase0::PluginsTest::Plugin.new(plugin).tests.each { |test| check_test(test) }
+    end
+
+    def check_test(test)
+      infom "Checking test \"#{test}\"..."
+      test.test_result
+      infom "Test \"#{test}\" checked"
+      @tests << test
       results_banner
     end
 
     def results_banner
-      infom 'Plugins\' test results:'
-      @plugins.each do |plugin|
-        infov "  * #{plugin.id}", plugin.test_result.label
+      infom 'Tests\' results:'
+      @tests.each do |test|
+        infov "  * #{test}", test.test_result.label
       end
     end
 
-    def plugins_failed
-      @plugins.select { |plugin| plugin.test_result.error? }
+    def tests_failed
+      @tests.select { |test| test.test_result.error? }
     end
 
     def check_results
-      if plugins_failed.any?
+      if tests_failed.any?
         fatal_error "Some test did not pass:\n" +
-                    plugins_failed.map { |p| "  * #{p.id} (Log: #{p.stdout_log})" }.join("\n")
+                    tests_failed.map { |test| "  * #{test} (Log: #{test.stdout_log})" }.join("\n")
       else
         success 'All tests passed'
-      end
-    end
-
-    class PluginTest < ::SimpleDelegator
-      include ::EacRubyUtils::SimpleCache
-
-      def initialize(plugin)
-        super(plugin)
-      end
-
-      def test_task_name
-        "#{id}:test"
-      end
-
-      def test_task?
-        ::Rake::Task.task_defined?(test_task_name)
-      end
-
-      def stderr_log
-        log_path('stderr')
-      end
-
-      def stdout_log
-        log_path('stdout')
-      end
-
-      def maintained?
-        ::EacRedmineBase0.maintained_plugins.any? { |plugin| plugin.id == id }
-      end
-
-      private
-
-      def log_path(suffix)
-        r = ::Rails.root.join('log', 'eac_redmine_base0', "#{id}_test_#{suffix}.log")
-        ::FileUtils.mkdir_p(::File.dirname(r))
-        r
-      end
-
-      def test_result_uncached
-        return ::Avm::Result.neutral('not maintained by E.A.C.') unless maintained?
-        return ::Avm::Result.neutral("task \"#{test_task_name}\" not found") unless test_task?
-
-        ::Avm::Result.success_or_error(
-          run_test, 'success', "failed (Log: #{stdout_log})"
-        )
-      end
-
-      def rails_gem_uncached
-        ::EacRubyGemsUtils::Gem.new(::Rails.root)
-      end
-
-      def run_test_uncached
-        r = run_test_command.execute
-        ::File.write(stderr_log, r.fetch(:stderr))
-        ::File.write(stdout_log, r.fetch(:stdout))
-        r.fetch(:exit_code).zero?
-      end
-
-      def run_test_command
-        rails_gem.bundle('exec', 'rake', test_task_name).envvar('RAILS_ENV', 'test')
       end
     end
   end
---
+++ plugins/eac_redmine_base0/lib/eac_redmine_base0/plugins_test/plugin.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'avm/instances/configuration' # TO-DO: remove when fixed in gem "avm-tools"
+require 'avm/patches/eac_ruby_gems_utils/gem'
+require 'avm/result'
+require 'eac_ruby_utils/simple_cache'
+
+module EacRedmineBase0
+  class PluginsTest
+    class Plugin < ::SimpleDelegator
+      include ::EacRubyUtils::SimpleCache
+
+      def initialize(plugin)
+        super(plugin)
+      end
+
+      def maintained?
+        ::EacRedmineBase0.maintained_plugins.any? { |plugin| plugin.id == id }
+      end
+
+      def tests
+        return [test_by_type(:unmaintened_stub)] unless maintained?
+
+        [test_by_type(:rake_task), test_by_type(:rubocop)]
+      end
+
+      private
+
+      def test_by_type(type)
+        ::EacRedmineBase0::PluginsTest.const_get(type.to_s.camelize + 'Test').new(self)
+      end
+
+      def plugin_gem_uncached
+        ::EacRubyGemsUtils::Gem.new(directory)
+      end
+
+      def rails_gem_uncached
+        ::EacRubyGemsUtils::Gem.new(::Rails.root)
+      end
+    end
+  end
+end
