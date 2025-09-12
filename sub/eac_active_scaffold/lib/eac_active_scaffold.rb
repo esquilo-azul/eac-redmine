4475851cdc2005b014498d8ab3f30ef8dad076c1
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2025-09-12 12:37:58 -0300

    Sub[sub/eac_active_scaffold]: remove.

--- sub/eac_active_scaffold/lib/eac_active_scaffold.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module EacActiveScaffold
-end
-
-require 'eac_active_scaffold/engine'
--- sub/eac_active_scaffold/lib/eac_active_scaffold/patches.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'eac_ruby_utils/core_ext'
-
-module EacActiveScaffold
-  module Patches
-    require_sub __FILE__, base: nil, require_dependency: false
-  end
-end
