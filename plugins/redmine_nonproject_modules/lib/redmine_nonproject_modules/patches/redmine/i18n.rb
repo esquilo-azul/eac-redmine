c0162e2ed089cab1667a2879212dfd0852820e3b
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-27 15:49:55 -0300

    Sub[plugins/redmine_nonproject_modules]: atualiza para '0.6.1'.

--- plugins/redmine_nonproject_modules/lib/redmine_nonproject_modules/patches/redmine/i18n.rb
+++ plugins/redmine_nonproject_modules/lib/redmine_nonproject_modules/patches/redmine/i18n.rb
@@ -5,7 +5,7 @@ require 'redmine/i18n'
 module RedmineNonprojectModules
   module Patches
     module Redmine
-      module I18nPatch
+      module I18n
         common_concern do
           alias_method :original_l, :l
           alias_method :l, :fixed_l
@@ -13,7 +13,8 @@ module RedmineNonprojectModules
 
         def fixed_l(*args)
           if (args.first.is_a?(Time) || args.first.is_a?(Date)) && args.last.is_a?(Hash)
-            ::I18n.l(args.first, args.last)
+            options = args.extract_options!
+            ::I18n.l(*args, **options)
           else
             original_l(*args)
           end
@@ -23,7 +24,4 @@ module RedmineNonprojectModules
   end
 end
 
-x = Redmine::I18n
-y = RedmineNonprojectModules::Patches::Redmine::I18nPatch
-
-x.send(:include, y) unless x.include?(y)
+Redmine::I18n.patch_self(RedmineNonprojectModules::Patches::Redmine::I18n)
