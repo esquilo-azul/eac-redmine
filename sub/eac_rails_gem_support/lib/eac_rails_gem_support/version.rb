f1a6a39a889d57c17fc8c46cef74a164568fe10c
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-07-29 14:43:22 -0300

    Sub[sub/eac_rails_gem_support]: remove.

--- sub/eac_rails_gem_support/lib/eac_rails_gem_support/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module EacRailsGemSupport
-  VERSION = '0.13.2'
-end
--- sub/eac_rails_gem_support/spec/features/home_page_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe 'Home page', :rails, type: :feature do
-  it 'visit home' do
-    visit '/'
-    expect(page).to have_content 'Home'
-  end
-end
