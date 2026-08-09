14ed11c1dcfb16808e5e560efec0585a3287efb0
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-09 00:02:51 -0300

    Bump to '0.4.9'.

--- plugins/redmine_tasks_scheduler/lib/redmine_tasks_scheduler/version.rb
+++ plugins/redmine_tasks_scheduler/lib/redmine_tasks_scheduler/version.rb
@@ -4,7 +4,7 @@ module RedmineTasksScheduler
   AUTHOR = 'Eduardo Henrique Bogoni'
   HOMEPAGE = 'https://github.com/esquilo-azul/redmine_tasks_scheduler'
   SUMMARY = 'Scheduler for rake tasks on Redmine.'
-  VERSION = '0.4.8'
+  VERSION = '0.4.9'
 
   module Version
   end
