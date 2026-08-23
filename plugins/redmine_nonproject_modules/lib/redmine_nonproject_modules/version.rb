143af28c9c93b40349c7898925b8fb0a0bc7b05f
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-08-22 21:53:43 -0300

    Bump to '0.6.5'.

--- plugins/redmine_nonproject_modules/lib/redmine_nonproject_modules/version.rb
+++ plugins/redmine_nonproject_modules/lib/redmine_nonproject_modules/version.rb
@@ -4,7 +4,7 @@ module RedmineNonprojectModules
   AUTHOR = 'Eduardo Henrique Bogoni'
   HOMEPAGE = 'https://github.com/esquilo-azul/redmine_nonproject_modules'
   SUMMARY = 'Support to non-project modules.'
-  VERSION = '0.6.4'
+  VERSION = '0.6.5'
 
   module Version
   end
