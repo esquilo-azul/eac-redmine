02827dd48dbc42dfe4fd2b07caca88cfcc4d1339
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-12 18:58:42 -0300

    Installer#$address_*: remove.

--- plugins/redmine_with_git/installer/default_settings.sh
+++ plugins/redmine_with_git/installer/default_settings.sh
@@ -1,6 +1,3 @@
-export address_https=false
-export address_host=localhost
-export address_port=
 export git_repositories_hierarchical_organisation=true
 export gitolite_user=git
 export gitolite_user_home=/var/lib/git
