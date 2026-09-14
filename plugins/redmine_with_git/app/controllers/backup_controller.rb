68a9ad1b04a5785eb7cd55bb8d118a029036e171
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-14 15:01:08 -0300

    BackupController.import: remove.

--- plugins/redmine_with_git/app/controllers/backup_controller.rb
+++ plugins/redmine_with_git/app/controllers/backup_controller.rb
@@ -1,45 +1,14 @@
 # frozen_string_literal: true
 
 class BackupController < ApplicationController
-  IMPORT_PERMISSION = 'redmine_with_git.backup.import'
-
-  PERMISSIONS = { or: [IMPORT_PERMISSION] }.freeze
+  PERMISSIONS = {}.freeze
 
   layout 'nonproject_modules'
   require_permission PERMISSIONS, only: [:index]
-  require_permission IMPORT_PERMISSION, only: [:import]
-
-  accept_api_auth :import
 
   helper ::RedmineWithGitHelper
 
   def index
     @load = ::RedmineWithGit::Tableless::Load.new
-  end
-
-  def import
-    @load = ::RedmineWithGit::Tableless::Load.new(import_params)
-    @load.save
-    respond_to do |format|
-      format.html { import_respond_to_html }
-      format.api { render_validation_errors(@load) }
-    end
-  end
-
-  private
-
-  def import_respond_to_html
-    if @load.errors.empty?
-      redirect_to backup_path, notice: 'Backup imported' # rubocop:disable Rails/I18nLocaleTexts
-    else
-      render :index
-    end
-  end
-
-  def import_params
-    ps = params[::RedmineWithGit::Tableless::Load.model_name.param_key]
-    return {} if ps.blank?
-
-    ps.permit(:path)
   end
 end
--- plugins/redmine_with_git/config/routes.rb
+++ plugins/redmine_with_git/config/routes.rb
@@ -2,5 +2,4 @@
 
 RedmineApp::Application.routes.draw do
   get '/backup', to: 'backup#index', as: 'backup'
-  post '/backup/import', to: 'backup#import', as: 'import_backup'
 end
