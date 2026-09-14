e1a292f97370c195af75ffee35392cf38cfc1111
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-14 15:20:06 -0300

    BackupController: remove.

--- plugins/redmine_with_git/config/initializers/500_nonproject_modules.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-Redmine::MenuManager.map :redmine_with_git do |menu|
-  menu.push_controller(:backup)
-end
--- plugins/redmine_with_git/config/routes.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-RedmineApp::Application.routes.draw do
-end
