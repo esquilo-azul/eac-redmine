e073401418c0ded60eacbdfb998a4e4693714d9a
Eduardo H. Bogoni <eduardobogoni@gmail.com>
2026-09-23 13:44:20 -0300

    Core: atualiza para '7.0.1'.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '>= 3.2.0', '< 4.1.0'
 
-gem 'rails', '8.1.3'
+gem 'rails', '8.1.3.1'
 gem 'rouge', '~> 5.0'
 gem 'mini_mime', '~> 1.1.0'
 gem "actionpack-xml_parser"
@@ -132,11 +132,4 @@ end
 # Load plugins' Gemfiles
 Dir.glob File.expand_path("../plugins/*/{Gemfile,PluginGemfile}", __FILE__) do |file|
   eval_gemfile file
-end
-
-# Subs
-Dir["#{File.join(__dir__, 'sub')}/*"].each do |dir|
-  next unless File.exist?(File.join(dir, "#{File.basename(dir)}.gemspec"))
-
-  gem File.basename(dir), path: dir, require: false
 end
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -232,7 +232,10 @@ class Attachment < ApplicationRecord
 
   def thumbnailable?
     Redmine::Thumbnail.convert_available? && (
-      image? || (is_pdf? && Redmine::Thumbnail.gs_available?)
+      image? ||
+        (Redmine::Thumbnail.gs_available? &&
+         # Illustrator files are often PDF compatible
+         ['application/pdf', 'application/illustrator'].include?(Redmine::MimeType.of(filename)))
     )
   end
 
@@ -581,7 +584,7 @@ class Attachment < ApplicationRecord
     just_filename = value.gsub(/\A.*(\\|\/)/m, '')
 
     # Finally, replace invalid characters with underscore
-    just_filename.gsub(/[\/?%*:|"'<>\n\r]+/, '_')
+    just_filename.gsub(/[\/?%*:|"'<>\n\r\x00]+/, '_')
   end
 
   # Returns the subdirectory in which the attachment will be saved
